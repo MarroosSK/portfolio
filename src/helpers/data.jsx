28fd78754d09f6798835a0344331320b1c5c39f4
MarroosSK <maros1chmel@gmail.com>
2023-10-23 14:27:49 +0200

    added new project to portfolio

--- src/helpers/data.jsx
+++ src/helpers/data.jsx
@@ -5,7 +5,10 @@ import gallery from "../assets/Gallery.jpg";
 import dictionary from "../assets/3_dictionary.jpg";
 import basics from "../assets/Reacty.jpg";
 import whyb from "../assets/whyb.jpg";
-import gamer from "../assets/11_gamer.jpg";
+import businessGO from "../assets/businessGO.jpg";
+import resumeBuilder from "../assets/resumeBuilder.jpg";
+import maGallery from "../assets/maGallery.jpg";
+
 //icons - social
 import { BsLinkedin, BsGithub } from "react-icons/bs";
 //icons - skillSet
@@ -20,13 +23,14 @@ import {
   SiMysql,
   SiPostgresql,
   SiTailwindcss,
+  SiNextdotjs,
 } from "react-icons/si";
 import { DiJavascript1 } from "react-icons/di";
 import { BsBootstrapFill } from "react-icons/bs";
 
 export const personal = {
   name: "Maroš Chmeľ",
-  info: `Hi! My name is Maroš Chmeľ. I am a self-taught frontend developer from Vranov nad Topľou, Slovakia. I work with React.js (frontend) and starting to learn Express (backend).`,
+  info: `Hi! My name is Maroš Chmeľ. I am a self-taught frontend developer from Vranov nad Topľou, Slovakia. I work with React.js (frontend) and starting to learn Next.Js (full-stack).`,
 };
 export const skillDesc = `While these are some of the technologies I am the most comfortable with when working with React, I am currently looking to expand my toolkit by learning new technologies!`;
 export const skillSet = [
@@ -101,9 +105,61 @@ export const skillSet = [
     name: "Prisma",
     icon: <SiPrisma size={25} />,
   },
+  {
+    id: 16,
+    name: "Next.js",
+    icon: <SiNextdotjs size={25} />,
+  },
 ];
 
 export const myProjects = [
+  {
+    id: uuidv4(),
+    title: "MA Gallery",
+    url: "https://ma-gallery.vercel.app",
+    info: "Create your own personal galleries. To log in with dummy credentials type jane+clerk_test@example.com in email input and 424242 in password input.",
+    source: "https://ma-gallery.vercel.app",
+    img: maGallery,
+    stack: [
+      "Next.Js",
+      "Typescript",
+      "ShadCN",
+      "Zustand",
+      "Axios",
+      "Clerk Auth",
+    ],
+    category: "full-stack",
+  },
+  {
+    id: uuidv4(),
+    title: "Resume Builder",
+    url: "https://cv-creator-nu.vercel.app",
+    info: "Create your own Resume. To log in with dummy credentials type jane+clerk_test@example.com in email input and 424242 in password input.",
+    source: "https://github.com/MarroosSK/cv_creator",
+    img: resumeBuilder,
+    stack: ["Next.Js", "Typescript", "ShadCN", "ContextAPI", "Axios"],
+    category: "full-stack",
+  },
+  {
+    id: uuidv4(),
+    title: "Business GO",
+    url: "https://business-listing-dusky.vercel.app",
+    info: "BusinessGO is app, that allows you to search for all local businesses using Yelp API.",
+    source: "https://github.com/MarroosSK/business-listing",
+    img: businessGO,
+    stack: ["Next.Js", "Typescript", "ShadCN", "Zustand", "Axios"],
+    category: "front-end",
+  },
+  {
+    id: uuidv4(),
+    title: "First 15 Gallery",
+    url: "https://8gallery.netlify.app/",
+    info: "Gallery app made with typescript and redux toolkit. Purpose of this app is to allow user search for pictures and display them in modal.",
+    source: "https://github.com/MarroosSK/8_gallery",
+    img: gallery,
+    stack: ["Typescript", "Material-UI", "Redux-Toolkit", "Axios"],
+    category: "front-end",
+  },
   {
     id: uuidv4(),
     title: "Where Have You Been? (Full-Stack)",
@@ -122,16 +178,6 @@ export const myProjects = [
       "React-Quill",
     ],
     category: "full-stack",
-  },
-  {
-    id: uuidv4(),
-    title: "First 15 Gallery",
-    url: "https://8gallery.netlify.app/",
-    info: "Gallery app made with typescript and redux toolkit. Purpose of this app is to allow user search for pictures and display them in modal.",
-    source: "https://github.com/MarroosSK/8_gallery",
-    img: gallery,
-    stack: ["Typescript", "Material-UI", "Redux-Toolkit", "Axios"],
-    category: "front-end",
   },
   {
     id: uuidv4(),
@@ -167,24 +213,6 @@ export const myProjects = [
       "React-Query",
       "Axios",
       "React-Icons",
-    ],
-    category: "front-end",
-  },
-  {
-    id: uuidv4(),
-    title: "Gamer - game keys store",
-    url: "https://gamerbymaros.netlify.app/",
-    info: "Gamer store app inspired in instant-gaming web app. Purpose of this app is to provide digital keys for games to users. It contains all functions gamer store app should have.",
-    source: "https://github.com/MarroosSK/11_gamarr",
-    img: gamer,
-    stack: [
-      "Typescript",
-      "Material-UI",
-      "ContextAPI",
-      "Axios",
-      "ChartJS",
-      "React-Hook-Form",
-      "React-Multicarousel",
     ],
     category: "front-end",
   },
